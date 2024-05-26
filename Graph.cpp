58e45727a2fc8d66e3f35a36c146af0d72d6ceae
DanielKuris <danielkuris6@gmail.com>
2024-05-26 15:54:49 +0300

    Assignment 2 additon

--- Graph.cpp
+++ Graph.cpp
@@ -1,23 +1,14 @@
-/*
-Email: danielkuris6@gmail.com
-ID: 214539397
-Name: Daniel Kuris
-*/
-#include "Graph.hpp" // Include the header file containing the class definition
-#include <iostream> // Include necessary standard library headers
-#include <vector>
+#include "Graph.hpp"
+#include <iostream>
+#include <stdexcept>
 
 namespace ariel {
     // Constructor
-    Graph::Graph() {
-        this->numVertices = 0;
-        this->numEdges = 0;
-    }
+    Graph::Graph() : numVertices(0), numEdges(0) {}
 
     // Destructor
-    Graph::~Graph() {
-        // Destructor implementation
-    }
+    Graph::~Graph() {}
+
     void Graph::loadGraph(const std::vector<std::vector<int>>& graph) {
         // Check if the graph is empty
         if (graph.empty()) {
@@ -44,12 +35,10 @@ namespace ariel {
         numEdges = calculateNumEdges(graph);
     }
 
-    // Method to print the graph information
     void Graph::printGraph() {
         std::cout << "Graph with " << numVertices << " vertices and " << numEdges << " edges." << std::endl;
     }
 
-    // Method to calculate the number of edges in the graph
     int Graph::calculateNumEdges(const std::vector<std::vector<int>>& graph) {
         int edges = 0;
         for (size_t i = 0; i < graph.size(); ++i) {
@@ -60,10 +49,9 @@ namespace ariel {
                 }
             }
         }
-        // Assuming that if an edge exists twice its an indirected graph thus should be counted
+        // Assuming that if an edge exists twice, it's an undirected graph and should be counted
         return edges;
     }
-
 
     std::vector<std::vector<int>> Graph::getGraph() const {
         return graph; // Return the adjacency matrix
@@ -81,4 +69,335 @@ namespace ariel {
         return graph[u][v] != 0; // Check if there is an edge between u and v
     }
 
+    // --------------------------------------------------------------
+
+    //PART B - OPERATORS
+
+    // Member function to check if the current graph is valid
+    bool Graph::validGraph() const {
+        // Check if the graph is empty
+        if (this->graph.empty()) {
+            throw std::invalid_argument("Invalid graph: The graph is empty.");
+            return false;
+        }
+
+        // Check if the graph is square
+        if (this->graph.size() != this->graph[0].size()) {
+            throw std::invalid_argument("Invalid graph: The graph is not a square matrix.");
+            return false;
+        }
+
+        // Check if the graph contains non-zero diagonal elements
+        for (size_t i = 0; i < this->graph.size(); ++i) {
+            if (this->graph[i][i] != 0) {
+                throw std::invalid_argument("Invalid graph: The graph contains non-zero diagonal elements.");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Operator +
+    Graph Graph::operator+(const Graph& other) const {
+        if (this->numVertices != other.numVertices) {
+            throw std::invalid_argument("Graphs must have the same dimensions to be added.");
+        }
+
+        std::vector<std::vector<int>> result = this->graph;
+
+        for (size_t i = 0; i < result.size(); ++i) {
+            for (size_t j = 0; j < result[i].size(); ++j) {
+                result[i][j] += other.graph[i][j];
+            }
+        }
+
+        Graph newGraph;
+        newGraph.loadGraph(result);
+        if (!newGraph.validGraph()) {
+            throw std::invalid_argument("Invalid graph after addition.");
+        }
+        return newGraph;
+    }
+
+    // Operator -
+    Graph Graph::operator-(const Graph& other) const {
+        if (this->numVertices != other.numVertices) {
+            throw std::invalid_argument("Graphs must have the same dimensions to be subtracted.");
+        }
+
+        std::vector<std::vector<int>> result = this->graph;
+
+        for (size_t i = 0; i < result.size(); ++i) {
+            for (size_t j = 0; j < result[i].size(); ++j) {
+                result[i][j] -= other.graph[i][j];
+            }
+        }
+
+        Graph newGraph;
+        newGraph.loadGraph(result);
+        if (!newGraph.validGraph()) {
+            throw std::invalid_argument("Invalid graph after subtraction.");
+        }
+        return newGraph;
+    }
+
+    // Operator +=
+    Graph& Graph::operator+=(const Graph& other) {
+        if (this->numVertices != other.numVertices) {
+            throw std::invalid_argument("Graphs must have the same dimensions to be added.");
+        }
+
+        for (size_t i = 0; i < this->graph.size(); ++i) {
+            for (size_t j = 0; j < this->graph[i].size(); ++j) {
+                this->graph[i][j] += other.graph[i][j];
+            }
+        }
+
+        // Recalculate the number of edges
+        this->numEdges = calculateNumEdges(this->graph);
+        if (!this->validGraph()) {
+            throw std::invalid_argument("Invalid graph after addition.");
+        }
+        return *this;
+    }
+
+    // Operator -=
+    Graph& Graph::operator-=(const Graph& other) {
+        if (this->numVertices != other.numVertices) {
+            throw std::invalid_argument("Graphs must have the same dimensions to be subtracted.");
+        }
+
+        for (size_t i = 0; i < this->graph.size(); ++i) {
+            for (size_t j = 0; j < this->graph[i].size(); ++j) {
+                this->graph[i][j] -= other.graph[i][j];
+            }
+        }
+
+        // Recalculate the number of edges
+        this->numEdges = calculateNumEdges(this->graph);
+        if (!this->validGraph()) {
+            throw std::invalid_argument("Invalid graph after subtraction.");
+        }
+        return *this;
+    }
+
+    // Unary Operator -
+    Graph Graph::operator-() const {
+        std::vector<std::vector<int>> result = this->graph;
+
+        for (size_t i = 0; i < result.size(); ++i) {
+            for (size_t j = 0; j < result[i].size(); ++j) {
+                result[i][j] *= -1;
+            }
+        }
+
+        Graph newGraph;
+        newGraph.loadGraph(result);
+        if (!newGraph.validGraph()) {
+            throw std::invalid_argument("Invalid graph after unary minus.");
+        }
+        return newGraph;
+    }
+
+    // Operator ++
+    Graph& Graph::operator++() {
+        for (size_t i = 0; i < this->numVertices; ++i) {
+            for (size_t j = 0; j < this->numVertices; ++j) {
+                if (this->graph[i][j] != 0) {
+                    this->graph[i][j]++;
+                }
+            }
+        }
+        // Recalculate the number of edges
+        this->numEdges = calculateNumEdges(this->graph);
+        if (!this->validGraph()) {
+            throw std::invalid_argument("Invalid graph after increment.");
+        }
+        return *this;
+    }
+
+    // Operator --
+    Graph& Graph::operator--() {
+        for (size_t i = 0; i < this->numVertices; ++i) {
+            for (size_t j = 0; j < this->numVertices; ++j) {
+                if (this->graph[i][j] != 0) {
+                    this->graph[i][j]--;
+                }
+            }
+        }
+        // Recalculate the number of edges
+        this->numEdges = calculateNumEdges(this->graph);
+        if (!this->validGraph()) {
+            throw std::invalid_argument("Invalid graph after decrement.");
+        }
+        return *this;
+    }
+
+    // Operator *
+    Graph& Graph::operator*(int scalar) {
+        for (size_t i = 0; i < this->numVertices; ++i) {
+            for (size_t j = 0; j < this->numVertices; ++j) {
+                this->graph[i][j] *= scalar;
+            }
+        }
+        // Recalculate the number of edges
+        this->numEdges = calculateNumEdges(this->graph);
+        if (!this->validGraph()) {
+            throw std::invalid_argument("Invalid graph after scalar multiplication.");
+        }
+        return *this;
+    }
+
+    // Operator *
+    Graph Graph::operator*(const Graph& other) const {
+        // Check if the graphs have the same number of vertices
+        if (this->numVertices != other.numVertices) {
+            throw std::invalid_argument("Invalid operation: Graphs have different number of vertices.");
+        }
+
+        // Initialize a new graph for the result
+        Graph result;
+        result.numVertices = this->numVertices;
+        result.graph.resize(this->numVertices, std::vector<int>(this->numVertices, 0));
+
+        // Perform matrix multiplication
+        for (size_t i = 0; i < this->numVertices; ++i) {
+            for (size_t j = 0; j < this->numVertices; ++j) {
+                for (size_t k = 0; k < this->numVertices; ++k) {
+                    result.graph[i][j] += this->graph[i][k] * other.graph[k][j];
+                }
+            }
+        }
+
+        // Ensure zero-diagonal
+        for (size_t i = 0; i < result.numVertices; ++i) {
+            result.graph[i][i] = 0;
+        }
+
+        if (!result.validGraph()) {
+            throw std::invalid_argument("Invalid graph after matrix multiplication.");
+        }
+
+        return result;
+    }
+
+       // Helper method to check if the current graph is fully contained within another graph
+    bool isContainedIn(const Graph& graph1, const Graph& graph2) {
+        if (graph1.getNumVertices() > graph2.getNumVertices()) return false;
+
+        size_t n = graph1.getNumVertices();
+        size_t m = graph2.getNumVertices();
+        const auto& g1 = graph1.getGraph();
+        const auto& g2 = graph2.getGraph();
+
+        for (size_t i = 0; i <= m - n; ++i) {
+            for (size_t j = 0; j <= m - n; ++j) {
+                bool match = true;
+                for (size_t k = 0; k < n; ++k) {
+                    for (size_t l = 0; l < n; ++l) {
+                        if (g1[k][l] != 0 && g1[k][l] != g2[i + k][j + l]) {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if (!match) break;
+                }
+                if (match) return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Operator <
+    bool Graph::operator<(const Graph& other) const {
+        if (isContainedIn(*this, other)) {
+            return true;
+        } else if (isContainedIn(other, *this)) {
+            return false;
+        } else {
+            return this->numEdges < other.numEdges;
+        }
+    }
+
+    // Operator >
+    bool Graph::operator>(const Graph& other) const {
+        if (isContainedIn(other, *this)) {
+            return true;
+        } else if (isContainedIn(*this, other)) {
+            return false;
+        } else {
+            return this->numEdges > other.numEdges;
+        }
+    }
+
+    // Operator ==
+    bool Graph::operator==(const Graph& other) const {
+        if (this->numVertices != other.numVertices) {
+            return false;
+        }
+
+        const auto& g1 = this->graph;
+        const auto& g2 = other.graph;
+
+        for (size_t i = 0; i < this->numVertices; ++i) {
+            for (size_t j = 0; j < this->numVertices; ++j) {
+                if (g1[i][j] != g2[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Operator !=
+    bool Graph::operator!=(const Graph& other) const {
+        return !(*this == other);
+    }
+    
+    // Operator <=
+    bool Graph::operator<=(const Graph& other) const {
+        return (*this < other) || (*this == other);
+    }
+
+    // Operator >=
+    bool Graph::operator>=(const Graph& other) const {
+        return (*this > other) || (*this == other);
+    }
+
+    // Member function to print a graphical representation of the graph
+    void Graph::visualGraph() const {
+        const auto& matrix = this->graph;
+        size_t numVertices = this->numVertices;
+
+        std::cout << "Visual Representation of the Graph:" << std::endl;
+
+        // Print column numbers
+        std::cout << "   ";
+        for (size_t i = 0; i < numVertices; ++i) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+
+        // Print horizontal line
+        std::cout << "  ";
+        for (size_t i = 0; i < numVertices * 2; ++i) {
+            std::cout << "-";
+        }
+        std::cout << std::endl;
+
+        // Print matrix
+        for (size_t i = 0; i < numVertices; ++i) {
+            // Print row number
+            std::cout << i << "| ";
+
+            // Print row elements
+            for (size_t j = 0; j < numVertices; ++j) {
+                std::cout << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 }
--- Test.cpp
+++ Test.cpp
@@ -288,3 +288,174 @@ TEST_CASE("Test shortestPath to non-existing verticle")
 }
 
 
+// --------------------- PART B TESTS ---------------------
+
+TEST_CASE("Test graph operators with simple cases")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {{0, 1, 0},
+                                   {1, 0, 1},
+                                   {0, 1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {{0, 0, 1},
+                                   {0, 0, 0},
+                                   {1, 0, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {{0, 2, 0},
+                                   {2, 0, 2},
+                                   {0, 2, 0}};
+    g3.loadGraph(graph3);
+
+    ariel::Graph result_addition = g1 + g2;
+    vector<vector<int>> expected_addition = {{0, 1, 1},
+                                              {1, 0, 1},
+                                              {1, 1, 0}};
+    CHECK(result_addition.getGraph() == expected_addition);
+
+    ariel::Graph result_subtraction = g1 - g2;
+    vector<vector<int>> expected_subtraction = {{0, 1, -1},
+                                                 {1, 0, 1},
+                                                 {-1, 1, 0}};
+    CHECK(result_subtraction.getGraph() == expected_subtraction);
+
+    ariel::Graph result_scalar_multiplication = g1 * 2;
+    vector<vector<int>> expected_scalar_multiplication = {{0, 2, 0},
+                                                          {2, 0, 2},
+                                                          {0, 2, 0}};
+    CHECK(result_scalar_multiplication.getGraph() == expected_scalar_multiplication);
+
+    ariel::Graph result_matrix_multiplication = g1 * g2;
+    vector<vector<int>> expected_matrix_multiplication = {{0, 0, 0},
+                                                          {1, 0, 1},
+                                                          {0, 0, 0}};
+    CHECK(result_matrix_multiplication.getGraph() == expected_matrix_multiplication);
+
+    ariel::Graph result_increment = g1++;
+    vector<vector<int>> expected_increment = {{0, 2, 0},
+                                              {2, 0, 2},
+                                              {0, 2, 0}};
+    CHECK(result_increment.getGraph() == expected_increment);
+
+    ariel::Graph result_decrement = g3--;
+    vector<vector<int>> expected_decrement = {{0, 1, 0},
+                                              {1, 0, 1},
+                                              {0, 1, 0}};
+    CHECK(result_decrement.getGraph() == expected_decrement);
+}
+
+TEST_CASE("Test graph equal operators")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {{0, 1, 0},
+                                   {1, 0, 1},
+                                   {0, 1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {{0, 1, 0},
+                                   {1, 0, 1},
+                                   {0, 1, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {{0, 0, 1},
+                                   {0, 0, 0},
+                                   {1, 0, 0}};
+    g3.loadGraph(graph3);
+
+    CHECK(g1 == g2);
+    CHECK(g1 != g3);
+}
+
+TEST_CASE("Test graph contain operators")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {{0, 1, 0},
+                                   {1, 0, 1},
+                                   {0, 1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {{0, 0, 1},
+                                   {0, 0, 0},
+                                   {1, 0, 0}};
+    g2.loadGraph(graph2);
+
+    CHECK(g1 > g2);
+    CHECK(g2 < g1);
+}
+
+TEST_CASE("Test graph contain/equal operators")
+{
+    //Taken from moodle forum
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {{0, 3},
+                                   {1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {{0, 4, 3},
+                                   {1, 0, -7},
+                                   {0, 6, 0}};
+    g2.loadGraph(graph2);
+
+    ariel::Graph g3;
+    vector<vector<int>> graph3 = {{0, 0, 1},
+                                   {1, 0, 1},
+                                   {1, 1, 0}};
+    g3.loadGraph(graph3);
+
+    CHECK(g1 <= g2);
+    CHECK(g3 >= g1);
+  
+}
+
+TEST_CASE("Test graph arithmetic operators")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph1 = {{0, 3},
+                                   {1, 0}};
+    g1.loadGraph(graph1);
+
+    ariel::Graph g2;
+    vector<vector<int>> graph2 = {{0, 4, 3},
+                                   {1, 0, -7},
+                                   {0, 6, 0}};
+    g2.loadGraph(graph2);
+
+    SUBCASE("Test addition operator") {
+        // Addition should throw an exception since graphs are of different sizes
+        CHECK_THROWS_AS(g1 + g2, std::invalid_argument);
+    }
+
+    SUBCASE("Test subtraction operator") {
+        // Subtraction should throw an exception since graphs are of different sizes
+        CHECK_THROWS_AS(g1 - g2, std::invalid_argument);
+    }
+
+    SUBCASE("Test scalar multiplication with zero") {
+        // Scalar multiplication with zero should throw an exception
+        CHECK_THROWS_AS(0 * g1, std::invalid_argument);
+    }
+
+    SUBCASE("Test multiplication operator") {
+        // Multiplication should throw an exception since graphs are of different sizes
+        CHECK_THROWS_AS(g1 * g2, std::invalid_argument);
+    }
+
+    SUBCASE("Test message after matrix multiplication") {
+        std::string errorMessage;
+        try {
+            ariel::Graph result = g1 * g2;
+        } catch (const std::invalid_argument& e) {
+            errorMessage = e.what();
+        }
+        // Check that the error message indicates invalid graph after matrix multiplication
+        CHECK(errorMessage.find("Invalid graph after matrix multiplication") != std::string::npos);
+    }
+
+}
